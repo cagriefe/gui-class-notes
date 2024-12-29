189e318b7b44c32225efe52dbfeca73f863a8b3f
cagriefe <cagriefe34@gmail.com>
2024-12-29 17:41:59 +0300

    week 14 content

---
+++ week14/week14d.py
@@ -0,0 +1,16 @@
+# Bar plot, custom colors
+
+import matplotlib.pyplot as plt
+
+x_values = ["A", "B", "C", "D"]
+y_values = [5, 7, 6, 8]
+colors = ["black", "green", "#0000FF", "brown"]
+
+fig, ax = plt.subplots()
+ax.bar(x_values, y_values, color=colors)
+
+ax.set_title("Colored Bar Plot")
+ax.set_xlabel("Letters")
+ax.set_ylabel("Numbers")
+
+plt.show()
---
+++ week14/week14e.py
@@ -0,0 +1,15 @@
+# Scatter plot
+
+import matplotlib.pyplot as plt
+
+x_values = [1, 2, 3, 4, 5]
+y_values = [2, 3, 5, 7, 11]
+
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values)
+
+ax.set_title("Scatter Plot")
+ax.set_xlabel("X Axis")
+ax.set_ylabel("Y Axis")
+
+plt.show()
